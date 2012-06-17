b59222e855cd2dde2a119a48fe93d00750dc8547
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-06-17 20:03:08 +0200

    BeforeEach introduced
    An beforeEach is introduced which creates a Ball.

--- spec/BallSpec.js
+++ spec/BallSpec.js
@@ -1,13 +1,15 @@
 describe("A Ball", function(){
-    it("should have a default position", function(){
-	var ball = new Ball();
+    var ball;
 
+    beforeEach(function(){
+	ball = new Ball();
+    });
+    
+    it("should have a default position", function(){
 	expect(ball).toBeAt({ x : 0, y : 0 });
     });
 
     it("should have a default velocity", function(){
-	var ball = new Ball();
-	
 	expect(ball.get("velocity").vx).toBe(1);
 	expect(ball.get("velocity").vy).toBe(1);
     });
